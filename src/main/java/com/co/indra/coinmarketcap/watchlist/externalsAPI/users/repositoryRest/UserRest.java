380d9b805921e70613ffba1eedd77fbda8c3824b
Juan Carlos Herrera Blandon <jhcarlos181@protonmail.com>
2022-06-09 15:17:27 -0500

    Se realiza creacion de codigo para crear cache en peticion a repositorio externo de usuarios con valor de un minuto

--- src/main/java/com/co/indra/coinmarketcap/watchlist/externalsAPI/users/repositoryRest/UserRest.java
+++ src/main/java/com/co/indra/coinmarketcap/watchlist/externalsAPI/users/repositoryRest/UserRest.java
@@ -5,6 +5,7 @@ import com.co.indra.coinmarketcap.watchlist.exceptions.NotFoundException;
 import com.co.indra.coinmarketcap.watchlist.externalsAPI.users.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -21,6 +22,7 @@ public class UserRest {
     @Value("${api.users.url}")
     private String apiUrl;
 
+    @Cacheable(value = "getUser", cacheManager = "expireOneMinute", key = "#id", unless = "#result == null")
     public UserModel getUserById(int id) {
         String url = apiUrl+id;
         ResponseEntity <UserModel> response = restTemplate.getForEntity(url, UserModel.class);
